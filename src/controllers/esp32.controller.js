b2885c80fbc92b3144f01c8432a293281ad2ac4e
Alexis De Leon <alexisdlr3009@hotmail.com>
2023-01-30 16:35:07 -0600

    Method changed to get

--- src/controllers/esp32.controller.js
+++ src/controllers/esp32.controller.js
@@ -1,8 +1,10 @@
 import Alarma from "../models/Alarma.js";
 export const esp32Controller = async (req, res) => {
-  const alarmaI = new Alarma(req.body);
-
+  const alarmaI = new Alarma();
+  const {estado} = req.params;
+  console.log(estado);
   try {
+    alarmaI.estado = estado;
     const alarma = await alarmaI.save();
     return res.json(alarma);
   } catch (error) {
--- src/routes/esp.routes.js
+++ src/routes/esp.routes.js
@@ -3,7 +3,7 @@ import { esp32Controller } from "../controllers/esp32.controller.js";
 const router = Router();
 
 //public routes
-router.post('/', esp32Controller)
+router.get('/:estado', esp32Controller)
 // private routes
 
 
